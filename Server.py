d0281d5f6de36729511d49ce9da7658154890663
René <rene.brueggemann@fh-muenster.de>
2016-04-04 16:48:10 +0200

    dissect message method

--- Server.py
+++ Server.py
@@ -12,42 +12,38 @@ class Server:
         self.port = port
         try:
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            print('socket successfully')
         except socket.error as msg:
             print('ERROR with socket: ' + str(msg))
 
     def start_server(self):
         try:
             self.socket.bind((self.host, self.port))
+            print('bind successfully')
         except socket.error as msg:
             print('ERROR with bind: ' + str(msg))
+
+        try:
+            self.socket.listen(5)
+            print('listen successfully')
+        except socket.error as msg:
+            print('ERROR with listen' + str(msg))
 
         self.accept_connections()
 
     def accept_connections(self):
         while True:
-            print('Wait for connections')
-            self.socket.listen(5)
-
             try:
                 conn, addr = self.socket.accept()
             except socket.error as msg:
                 print('ERROR with accept: ' + str(msg))
 
-            print('RECV')
+            print('receiving...')
 
             data = conn.recv(2048)
 
-            length = len(data)
-            print('First Byte : ' + str(length))
-
-            try:
-                # string = data.decode('ISO-8859-1')
-                string = struct.unpack(str(length) + 'c', data)  # repr(data)
-                # first_token = string.split(' ')[0]
-                # print('First token: ' + first_token)
-                print('Request    : ' + string)
-            except Exception as msg:
-                print('ERROR with decode: ' + str(msg))
+            self.dissect_message(data)
 
     def stop_server(self):
         try:
@@ -60,6 +56,9 @@ class Server:
         except socket.error as msg:
             print('ERROR with close: ' + str(msg))
 
+    def dissect_message(self, data):
+        print('message type\t: ' + struct.unpack('!B', data[0]))
+
 
 def gracefull_shutdown(sig, dummy):
     server.stop_server()
@@ -68,7 +67,7 @@ def gracefull_shutdown(sig, dummy):
 
 signal.signal(signal.SIGINT, gracefull_shutdown)
 
-server = Server('', 1337)
+server = Server('', 443)
 server.start_server()
 
 server.stop_server()
