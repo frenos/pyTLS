cb13ec54a08b258cbdf5eeaedb6f631a4a1b8370
frenos <frenos@users.noreply.github.com>
2016-04-04 21:24:27 +0200

    - added logging instead of ugly print :) - added first version of dissect message

--- Server.py
+++ Server.py
@@ -2,33 +2,50 @@ import signal
 import socket
 import struct
 import sys
+import logging
 
 
 class Server:
     TLS_HANDSHAKE = 22  # 0x16
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    fileHandler = logging.FileHandler('server.log')
+    fileHandler.setLevel(logging.DEBUG)
+
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fileHandler.setFormatter(formatter)
+    consoleHandler.setFormatter(formatter)
+
+    #logger.addHandler(fileHandler)
+    logger.addHandler(consoleHandler)
 
     def __init__(self, host='', port=443):
+
         self.host = host
         self.port = port
         try:
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            print('socket successfully')
+            Server.logger.debug('socket successfully')
         except socket.error as msg:
-            print('ERROR with socket: ' + str(msg))
+            Server.logger.error('ERROR with socket: %s' % msg)
 
     def start_server(self):
         try:
             self.socket.bind((self.host, self.port))
-            print('bind successfully')
+            Server.logger.debug('bind successfully')
         except socket.error as msg:
-            print('ERROR with bind: ' + str(msg))
+            Server.logger.error('ERROR with bind: %s' % msg)
 
         try:
             self.socket.listen(5)
-            print('listen successfully')
+            Server.logger.debug('listen successfully')
         except socket.error as msg:
-            print('ERROR with listen' + str(msg))
+            Server.logger.error('ERROR with listen: %s' % msg)
 
         self.accept_connections()
 
@@ -37,9 +54,9 @@ class Server:
             try:
                 conn, addr = self.socket.accept()
             except socket.error as msg:
-                print('ERROR with accept: ' + str(msg))
+                Server.logger.error('ERROR with accept: %s' %msg)
 
-            print('receiving...')
+            Server.logger.info('receiving...')
 
             data = conn.recv(2048)
 
@@ -49,15 +66,33 @@ class Server:
         try:
             self.socket.shutdown(socket.SHUT_RDWR)
         except socket.error as msg:
-            print('ERROR with shutdown: ' + str(msg))
+            Server.logger.error('ERROR with shutdown: %s' %msg)
 
         try:
             self.socket.close()
         except socket.error as msg:
-            print('ERROR with close: ' + str(msg))
+            Server.logger.error('ERROR with close: %s' % msg)
 
     def dissect_message(self, data):
-        print('message type\t: ' + struct.unpack('!B', data[0]))
+        Server.logger.debug('Try to dissect: %s' % data)
+        unpackedHeader = struct.unpack('!BHHB', data [:6])
+        '''Messagelength ist 3Byte, Python braucht fuer unpack aber 4Byte
+           daher hier 3Byte aus data + 1Byte extra padding am anfang
+        '''
+        paddingByte = '\x00'
+        unpackedMessageLength = struct.unpack('!I', paddingByte+data[6:9])
+        unpackedMessage = struct.unpack('!I', data[9:13])
+        myHello = {
+            'contentType' : hex(unpackedHeader[0]),
+            'tlsVersion' : hex(unpackedHeader[1]),
+            'length' : unpackedHeader[2],
+            'handshakeType' : unpackedHeader[3],
+            'messageLength' : unpackedMessageLength[0],
+            'messageVersion' : hex(unpackedMessage[0])
+        }
+
+        Server.logger.debug('dissected ClientHello: %s' % myHello)
+        return myHello
 
 
 def gracefull_shutdown(sig, dummy):
