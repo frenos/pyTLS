3618606b4c584fe572c5a25b5944635dc797f22d
René <rene.brueggemann@fh-muenster.de>
2016-04-11 14:32:41 +0200

    server hello

--- Server.py
+++ Server.py
@@ -1,8 +1,12 @@
 import logging
 import signal
 import socket
+import ssl
 import struct
 import sys
+import time
+
+import Parser
 
 
 class Server:
@@ -25,8 +29,9 @@ class Server:
     # logger.addHandler(fileHandler)
     logger.addHandler(consoleHandler)
 
-    def __init__(self, host='', port=443):
+    parser = Parser.Parser()
 
+    def __init__(self, host='', port=443):
         self.host = host
         self.port = port
         try:
@@ -53,6 +58,7 @@ class Server:
         self.accept_connections()
 
     def accept_connections(self):
+
         while True:
             try:
                 conn, addr = self.socket.accept()
@@ -63,7 +69,9 @@ class Server:
 
             data = conn.recv(2048)
 
-            self.dissect_message(data)
+            print(len(data))
+
+            self.dissect_message(conn, data)
 
     def stop_server(self):
         try:
@@ -78,63 +86,136 @@ class Server:
 
     """ DISSECT EVERY MESSAGE HERE """
 
-    def dissect_message(self, data):
-        Server.logger.debug('Try to dissect raw packet: %s' % data)
+    def dissect_message(self, conn, data):
+        Server.logger.debug('Try to dissect raw packet')
 
-        unpackedHeader = struct.unpack('!BHHB', data[:6])
-        '''Messagelength ist 3Byte, Python braucht fuer unpack aber 4Byte
-           daher hier 3Byte aus data + 1Byte extra padding am anfang
-        '''
+        header = self.parser.get_parsed_record_layer_header(data)
 
-        if unpackedHeader[0] == self.TLS_HANDSHAKE:
-            self.dissect_handshake_msg(unpackedHeader, data)
+        if header['content_type'] == self.TLS_HANDSHAKE:
+            self.dissect_handshake(conn, data)
 
     """ DISSECT HANDSAKE HERE """
 
-    def dissect_handshake_msg(self, unpackedHeader, data):
+    def dissect_handshake(self, conn, data):
         Server.logger.debug('Try to dissect handshake')
 
-        if unpackedHeader[3] == self.TYPE_CLIENT_HELLO:
-            self.dissect_client_hello(unpackedHeader, data)
+        ssl_data = self.parser.get_ssl_data(data)
 
-    """ DISSECT CLIENT_HELLO HERE """
+        print(ssl_data)
 
-    def dissect_client_hello(self, unpackedHeader, data):
-        Server.logger.debug('Try to dissect client_hello')
+        if ssl_data['handshake_type'] == self.TYPE_CLIENT_HELLO:
+            print(' CLIENT HELLO')
+            server_hello = self.pack_server_hello(ssl_data)
+            conn.send(server_hello)
+            print('DONE')
 
-        paddingByte = b'\x00'
-        unpackedMessageLength = struct.unpack('!I', paddingByte + data[6:9])
-        unpackedMessage = struct.unpack('!HI' + 'B' * 28 + 'BH', data[9:46])
+    def pack_server_hello(self, ssl_data):
+        Server.logger.debug('pack server_hello here')
 
-        # concat randomBytes aus der Message
-        randomBytes = bytearray()
-        for i in range(2, 30):
-            randomBytes.append(unpackedMessage[i])
+        buf = bytes()
+        message_length = 0
+        handshake_protocol_length = 0
+        handshake_protocol_length_bytes = bytes()
+        message_length_bytes = bytes()
 
-        ciphersuitesLength = int(unpackedMessage[31])
-        # get ciphersuites
-        unpackedCiphers = struct.unpack('!' + 'H' * int(ciphersuitesLength / 2), data[46:46 + ciphersuitesLength])
-        availableCipers = []
-        for cipher in unpackedCiphers:
-            availableCipers.append(hex(cipher))
+        stack = []
 
-        # get compressionLength
+        extension, extension_length = self.create_server_hello_extension(ssl_data)
+        handshake_protocol_length += extension_length
+        stack.append(extension)
 
-        myHello = {
-            'contentType': hex(unpackedHeader[0]),
-            'tlsVersion': hex(unpackedHeader[1]),
-            'length': unpackedHeader[2],
-            'handshakeType': unpackedHeader[3],
-            'messageLength': unpackedMessageLength[0],
-            'messageVersion': hex(unpackedMessage[0]),
-            'randomTimestamp': unpackedMessage[1],
-            'randomBytes': str(randomBytes),
-            'SessionIDLength': unpackedMessage[30],
-            'CiphersuitesLength': ciphersuitesLength,
-            'Ciphersuites': availableCipers
-        }
+        extension_length = struct.pack('!H', extension_length)
+        handshake_protocol_length += 2
+        stack.append(extension_length)
 
-        Server.logger.debug('dissected ClientHello: %s' % myHello)
+        compression_method = struct.pack('!B', 0)
+        handshake_protocol_length += 1
+        stack.append(compression_method)
+
+        cipher_suites = struct.pack('!H', 49202)
+        handshake_protocol_length += 2
+        stack.append(cipher_suites)
+
+        session_id_length = struct.pack('!B', 0)
+        handshake_protocol_length += 1
+        stack.append(session_id_length)
+
+        random_bytes = ssl.RAND_bytes(28)
+        handshake_protocol_length += 28
+        stack.append(random_bytes)
+
+        print(str(random_bytes))
+
+        timestamp = struct.pack('!I', int(time.time()))
+        handshake_protocol_length += 4
+        stack.append(timestamp)
+
+        max_tls_version = struct.pack('!H', 771)
+        handshake_protocol_length += 2
+        stack.append(max_tls_version)
+
+        for i in (16, 8, 0):
+            hexa = (handshake_protocol_length >> i) & 0xff
+
+            handshake_protocol_length_bytes += struct.pack('!B', hexa)
+
+        stack.append(handshake_protocol_length_bytes)
+        message_length = handshake_protocol_length
+        message_length += 3
+
+        handshake_protocol = struct.pack('!B', 2)
+        message_length += 1
+        stack.append(handshake_protocol)
+
+        message_length_bytes = struct.pack('!H', message_length)
+        stack.append(message_length_bytes)
+
+        min_tls_version = struct.pack('!H', 771)
+        stack.append(min_tls_version)
+
+        content_type = struct.pack('!B', 22)
+        stack.append(content_type)
+
+        while len(stack) != 0:
+            buf += stack.pop()
+
+        return buf
+
+    def create_server_hello_extension(self, ssl_data):
+
+        extension_buf = bytes()
+        extension_length = 0
+
+        server_name = 0
+        server_name_data_length = 0
+        extension_length += 4
+        extension_buf += struct.pack('!HH', server_name, server_name_data_length)
+
+        renegotiation_info = 65281
+        renegotiation_info_data_length = 1
+        renegotiation_info_extension_length = 0
+        extension_length += (4 + renegotiation_info_data_length)
+        extension_buf += struct.pack('!HHB', renegotiation_info, renegotiation_info_data_length,
+                                     renegotiation_info_extension_length)
+
+        ec_point_formats = 11
+        ec_point_formats_data_length = 3
+        ec_point_formats_length = 2
+        ec_point_format_uncompress = 0
+        ec_point_format_anisx962_compressed_prime = 1
+        extension_length += (4 + ec_point_formats_data_length)
+        extension_buf += struct.pack('!HHBBB', ec_point_formats, ec_point_formats_data_length,
+                                     ec_point_formats_length, ec_point_format_uncompress,
+                                     ec_point_format_anisx962_compressed_prime)
+
+        session_ticket = 35
+        session_ticket_data_length = 0
+        extension_length += (4 + session_ticket_data_length)
+        extension_buf += struct.pack('!HH', session_ticket, session_ticket_data_length)
+
+        print('create extension')
+
+        return extension_buf, extension_length
 
 
 ########################################################################################################################
@@ -147,7 +228,7 @@ def gracefull_shutdown(sig, dummy):
 
 signal.signal(signal.SIGINT, gracefull_shutdown)
 
-server = Server('', 1337)
+server = Server('', 443)
 server.start_server()
 
 server.stop_server()
